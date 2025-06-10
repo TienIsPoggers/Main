10e8a2b08dcda1f401d7157bf616b13a9614417b
TienIsPoggers <hangquoctien2008@gmail.com>
2025-06-10 23:52:12 +0700

    08 commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/routes.js');
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'./public')))
 app.use('/',routes)
