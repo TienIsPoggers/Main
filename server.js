3c5dd426a99f67da3271d99b65e606d8e7f45553
TienIsPoggers <hangquoctien2008@gmail.com>
2025-06-10 23:59:49 +0700

    09 commit

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/routes.js');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'./public')))
+app.get('/', (req, res) => {
+  res.send('Server is live! Welcome to the backend!');
+});
 app.use('/',routes)
 app.use((req,res)=>{
     res.redirect('/404');
